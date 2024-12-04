57ff36928fd8765ff9e017e03edb95bfc99a1ed4
Venkat <bodempudi@users.noreply.github.com>
2024-12-04 23:29:26 +0530

    Add files via upload

---
+++ ExecuteCatalogPackageTaskComplexUI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Dts.Runtime;
+using Microsoft.SqlServer.Dts.Runtime.Design;
+using System.Windows.Forms;
+
+namespace ExecuteCatalogPackageTaskComplexUI
+{
+    public class ExecuteCatalogPackageTaskComplexUI : IDtsTaskUI
+    {
+        // Public key token: a68173515d1ee3e3
+
+        private TaskHost taskHost = null;
+        private IDtsConnectionService connectionService = null;
+
+        public void Initialize(TaskHost taskHost, IServiceProvider serviceProvider)
+        {
+            this.taskHost = taskHost;
+            this.connectionService = serviceProvider.GetService(typeof(IDtsConnectionService)) as IDtsConnectionService;
+        }
+
+        public ContainerControl GetView()
+        {
+            return new ExecuteCatalogPackageTaskComplexUIForm(taskHost, connectionService);
+        }
+
+        public void New(IWin32Window parentWindow)
+        {
+            // throw new NotImplementedException();
+        }
+
+        public void Delete(IWin32Window parentWindow)
+        {
+            // throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ExecuteCatalogPackageTaskComplexUIForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.DataTransformationServices.Controls;
+using System.Drawing;
+using Microsoft.SqlServer.Dts.Runtime;
+
+namespace ExecuteCatalogPackageTaskComplexUI
+{
+    public partial class ExecuteCatalogPackageTaskComplexUIForm : DTSBaseTaskUI
+    {
+        private const string Title = "Execute Catalog Package Task Editor";
+        private const string Description = "This task executes an SSIS package in an SSIS Catalog.";
+        public static Icon taskIcon = new Icon(typeof(ExecuteCatalogPackageTask.ExecuteCatalogPackageTask), "ALCStrike.ico");
+
+        public ExecuteCatalogPackageTaskComplexUIForm(TaskHost taskHost, object connections) :
+         base(Title, taskIcon, Description, taskHost, connections)
+        {
+            // Add General view
+            GeneralView generalView = new GeneralView();
+            this.DTSTaskUIHost.AddView("General", generalView, null);
+            // Add Settings view
+            SettingsView settingsView = new SettingsView();
+            this.DTSTaskUIHost.AddView("Settings", settingsView, null);
+        }
+    }
+}
